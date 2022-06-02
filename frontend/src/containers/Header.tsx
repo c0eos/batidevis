af2006d8e1bc621dd0f583b263a287e515e9d2e4
Valentin Dubois <valentin.dubois60@gmail.com>
2022-06-02 17:18:53 +0200

    fontawesome

--- frontend/src/containers/Header.tsx
+++ frontend/src/containers/Header.tsx
@@ -5,31 +5,59 @@ export default function Header() {
   const user = useAppSelector((state) => state.user);
 
   const activeStyle = ({ isActive } : {isActive:boolean}) => {
-    const basestyle = "px-2";
+    const basestyle = "px-4";
     if (isActive) {
-      return `${basestyle} text-red-600`;
+      return `${basestyle} text-black font-bold`;
     }
-    return basestyle;
+    return `${basestyle} text-gray-500`;
   };
 
   return (
     <nav className="flex flex-row py-4 bg-slate-100">
-      <Link to="/" className="px-2">Accueil</Link>
 
-      <span className="grow" />
-
-      <div className="flex flex-row justify-evenly grow">
-        {user.isLoggedIn && <NavLink to="/clients" className={activeStyle}>Clients</NavLink>}
-        {user.isLoggedIn && <NavLink to="/devis" className={activeStyle}>Devis</NavLink>}
-        {user.isLoggedIn && <NavLink to="/factures" className={activeStyle}>Factures</NavLink>}
-        {user.isLoggedIn && <NavLink to="/acomptes" className={activeStyle}>Acomptes</NavLink>}
+      <div className="grow">
+        {user.isLoggedIn && (
+        <NavLink to="/clients" className={activeStyle}>
+          <i className="pr-1 fa-solid fa-user-group" />
+          Clients
+        </NavLink>
+        )}
+        {user.isLoggedIn && (
+        <NavLink to="/devis" className={activeStyle}>
+          <i className="pr-1 fa-solid fa-file-invoice" />
+          Devis
+        </NavLink>
+        )}
+        {user.isLoggedIn && (
+        <NavLink to="/factures" className={activeStyle}>
+          <i className="pr-1 fa-solid fa-file-invoice-dollar" />
+          Factures
+        </NavLink>
+        )}
+        {user.isLoggedIn && (
+        <NavLink to="/acomptes" className={activeStyle}>
+          <i className="pr-1 fa-solid fa-coins" />
+          Acomptes
+        </NavLink>
+        )}
       </div>
 
       <span className="grow" />
 
       {!user.isLoggedIn && <Link to="/login" className="px-2">Se Connecter</Link>}
-      {user.isLoggedIn && <Link to="/settings" className="px-2">Paramètres</Link>}
-      {user.isLoggedIn && <Link to="/logout" className="px-2">Se déconnecter</Link>}
+      {user.isLoggedIn && (
+      <NavLink to="/settings" className={activeStyle}>
+
+        <i className="pr-1 fa-solid fa-gear" />
+        Paramètres
+      </NavLink>
+      )}
+      {user.isLoggedIn && (
+      <NavLink to="/logout" className={activeStyle}>
+        <i className="pr-1 fa-solid fa-right-from-bracket" />
+        Se déconnecter
+      </NavLink>
+      )}
     </nav>
   );
 }
